c035876fd3dd7e33d5fbcc5e8264aa9335f54dae
Juuso Ansaharju <juuso.ansaharju@gofore.com>
2016-09-12 16:44:25 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+const StorePicker = React.createClass({
+  render: () => {
+    return (
+      <p>hello</p>
+    )
+  }
+})
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'))
