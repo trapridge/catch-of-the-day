aee4ed8413904c8deaf4cacfe9b4567115e586ae
Juuso Ansaharju <juuso.ansaharju@gofore.com>
2016-09-13 22:35:43 +0300

    Update to chapter 13

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,25 +1,40 @@
-let helpers =  {
-  formatPrice :  function(cents) {
-    return '$' + ( (cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") );
-  },
-  rando : function(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-  },
-  slugify : function(text) {
+export default class Helpers {
+  static formatPrice(cents) {
+    return '$' + 
+      ((cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+  }
+
+  static rnd(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+  }
+
+  static slugify(text) {
     return text.toString().toLowerCase()
       .replace(/\s+/g, '-')           // Replace spaces with -
       .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
       .replace(/\-\-+/g, '-')         // Replace multiple - with single -
       .replace(/^-+/, '')             // Trim - from start of text
-      .replace(/-+$/, '');            // Trim - from end of text
-  },
-  getFunName : function() {
-    var adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 'fancy', 'glamorous', 'handsome', 'long', 'magnificent', 'old-fashioned', 'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 'bewildered', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 'helpless', 'itchy', 'jealous', 'lazy', 'mysterious', 'nervous', 'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 'worried'];
+      .replace(/-+$/, '')             // Trim - from end of text
+  }
+
+  static getFunName() {
+    const adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 
+      'fancy', 'glamorous', 'handsome', 'long', 'magnificent', 'old-fashioned', 
+      'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 
+      'bewildered', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 
+      'helpless', 'itchy', 'jealous', 'lazy', 'mysterious', 'nervous', 
+      'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 
+      'worried'];
+    const nouns = ['women', 'men', 'children', 'teeth', 'feet', 'people', 
+      'leaves', 'mice', 'geese', 'halves', 'knives', 'wives', 'lives', 'elves', 
+      'loaves', 'potatoes', 'tomatoes', 'cacti', 'foci', 'fungi', 'nuclei', 
+      'syllabuses', 'analyses', 'diagnoses', 'oases', 'theses', 'crises', 
+      'phenomena', 'criteria', 'data'];
     
-    var nouns = ['women', 'men', 'children', 'teeth', 'feet', 'people', 'leaves', 'mice', 'geese', 'halves', 'knives', 'wives', 'lives', 'elves', 'loaves', 'potatoes', 'tomatoes', 'cacti', 'foci', 'fungi', 'nuclei', 'syllabuses', 'analyses', 'diagnoses', 'oases', 'theses', 'crises', 'phenomena', 'criteria', 'data'];
-    
-    return `${this.rando(adjectives)}-${this.rando(adjectives)}-${this.rando(nouns)}`;
+    return [
+      Helpers.rnd(adjectives), 
+      Helpers.rnd(adjectives),
+      Helpers.rnd(nouns)
+    ].join('-')
   }
 }
-
-export default helpers;
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,189 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
+import Helpers from './helpers'
+import { samples } from './sample-fishes'
 
-const StorePicker = React.createClass({
-  render: () => {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.addFish = this.addFish.bind(this)
+    this.loadSamples = this.loadSamples.bind(this)
+    this.renderFish = this.renderFish.bind(this)
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish) {
+    const timestamp = (new Date()).getTime()
+    this.state.fishes['fish-' + timestamp] = fish
+    this.setState({ 
+      fishes: this.state.fishes 
+    })
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: samples
+    }) 
+  }
+
+  render() {
     return (
-      <p>hello</p>
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="whatever"/>
+          <ul className="list-of-fishes">
+           {Object.keys(this.state.fishes).map(this.renderFish)} 
+          </ul>
+        </div>
+        <Order/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+      </div>
     )
   }
-})
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'))
+  renderFish(key) {
+    return (
+      <Fish key={key} index={key} details={this.state.fishes[key]}/>
+    )
+  }
+}
+
+class Fish extends React.Component {
+  render() {
+    var details = this.props.details
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{Helpers.formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+      </li>
+    )
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="top">
+        <h1>
+          Catch 
+          <span className="ofThe">
+            <span className="of">of</span> 
+            <span className="the">the</span>
+          </span> 
+          day
+        </h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
+    )
+  }
+}
+  
+class Order extends React.Component {
+  render() {
+    return (
+      <p>order</p>
+    )
+  }
+}
+
+class Inventory extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Inventory</h2>
+        <AddFishForm {...this.props}/>
+        <button onClick={this.props.loadSamples}>Load sample fishes</button>
+      </div>
+    )
+  }
+}
+
+class AddFishForm extends React.Component {
+  constructor() {
+    super()
+    this.createFish = this.createFish.bind(this)
+  }
+
+  render() {
+    return (
+      <form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
+        <input type="text" ref="name" placeholder="Fish name"/>
+        <input type="text" ref="price" placeholder="Fish price"/>
+        <select ref="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold out!</option>
+        </select>
+        <textarea type="text" ref="desc" placeholder="Desc"></textarea>
+        <input type="text" ref="image" placeholder="URL to image"/>
+        <button type="submit">+ Add item</button>
+      </form>
+    )
+  }
+
+  createFish(event) {
+    event.preventDefault()
+    const fish = { 
+      name: this.refs.name.value,
+      price: this.refs.price.value,
+      status: this.refs.status.value,
+      desc: this.refs.desc.value,
+      image: this.refs.image.value
+    }
+    this.props.addFish(fish)
+    this.refs.fishForm.reset()
+  }
+}
+
+class StorePicker extends React.Component {
+  constructor() {
+    super()
+    this.goToStore = this.goToStore.bind(this)
+  }
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        {/* comment */}
+        <h2>Please enter a store</h2>
+        <input type="text" 
+                ref="storeId" 
+                defaultValue={Helpers.getFunName()} 
+                required />
+        <input type="submit" />
+      </form>
+    )
+
+  }
+
+  goToStore(event) {
+    event.preventDefault()
+    browserHistory.push(`/store/${this.refs.storeId.value}`)
+  }
+}
+
+class NoMatch extends React.Component {
+  render() {
+    return (
+      <h1>404</h1>
+    )
+  }
+}
+ 
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+    <Route path="*" component={NoMatch}/>
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'))
+
